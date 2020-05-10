0af5a05f45552286bf4aba6575e69a11a803d9b3
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-05-10 04:12:06 -0400

    Initial Commit

---
+++ Scarlet.Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scarlet.Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
