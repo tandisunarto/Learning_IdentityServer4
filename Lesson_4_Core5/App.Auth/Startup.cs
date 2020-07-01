ff2ec8f0430eb802148f90fef6c052e50b65074f
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-07-01 02:20:25 -0400

    add external providers (google/facebook)

--- Lesson_4_Core5/App.Auth/Startup.cs
+++ Lesson_4_Core5/App.Auth/Startup.cs
@@ -67,8 +67,13 @@ namespace App.Auth
                     // register your IdentityServer with Google at https://console.developers.google.com
                     // enable the Google+ API
                     // set the redirect URI to https://localhost:5001/signin-google
-                    options.ClientId = "copy client ID from Google here";
-                    options.ClientSecret = "copy client secret from Google here";
+                    options.ClientId = "911811550081-c9c9i1ja3mrcu301qck56d3paen1c7dm.apps.googleusercontent.com";
+                    options.ClientSecret = "ioK_NTskcJJfXy6VaveF5MiJ";
+                })
+                .AddFacebook(options =>
+                {
+                    options.AppId = "2600433903540231";
+                    options.AppSecret = "e64a7bc262991f8c1e9d30b3e0b3b886";
                 });
         }
 
