98125dda870c2fe281cc7b7f39887c93829c6966
Tandi Sunarto <tandi.sunarto@hotmail.com>
2020-07-02 01:05:31 -0400

    Update client

--- Lesson_4_Core5/App.Auth/Config.cs
+++ Lesson_4_Core5/App.Auth/Config.cs
@@ -10,11 +10,11 @@ namespace App.Auth
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-                   new IdentityResource[]
-                   {
+            new IdentityResource[]
+            {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile(),
-                   };
+            };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
@@ -41,14 +41,14 @@ namespace App.Auth
                 // interactive client using code flow + pkce
                 new Client
                 {
-                    ClientId = "interactive",
+                    ClientId = "app.mvc",
                     ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
 
                     AllowedGrantTypes = GrantTypes.Code,
 
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:9011/signin-oidc" },
+                    FrontChannelLogoutUri = "https://localhost:9011/signout-oidc",
+                    PostLogoutRedirectUris = { "https://localhost:9011/signout-callback-oidc" },
 
                     AllowOfflineAccess = true,
                     AllowedScopes = { "openid", "profile", "scope2" }
